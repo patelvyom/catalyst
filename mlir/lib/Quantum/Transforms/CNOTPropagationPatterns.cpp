49af3703edb1e868be3f6fb6ca6c8c3b0618bf96
Vyom Patel <vyom.patel@uwaterloo.ca>
2024-12-10 18:30:11 +0530

    add match propagation logic

--- mlir/lib/Quantum/Transforms/CNOTPropagationPatterns.cpp
+++ mlir/lib/Quantum/Transforms/CNOTPropagationPatterns.cpp
@@ -12,6 +12,7 @@ using llvm::dbgs;
 using namespace mlir;
 using namespace catalyst::quantum;
 
+static const mlir::StringSet<> PropagationOps = {"Hadamard", "PauliX", "PauliY", "PauliZ"};
 namespace {
 
 struct CNOTPropagationRewritePattern : public mlir::OpRewritePattern<CustomOp> {
@@ -24,17 +25,39 @@ struct CNOTPropagationRewritePattern : public mlir::OpRewritePattern<CustomOp> {
         if (opGateName != "CNOT")
             return failure();
 
-        VerifyParentGateAnalysis vpga(op);
-        if (!vpga.getVerifierResult())
-            return failure();
         auto parentOp = cast<CustomOp>(op.getInQubits()[0].getDefiningOp());
         StringRef parentGateName = parentOp.getGateName();
-        return failure();
-        if (parentGateName != "CNOT")
+        if (!PropagationOps.contains(parentGateName))
             return failure();
+        
+        ValueRange inNonCtrlQubits = op.getNonCtrlQubitOperands();
+        ValueRange inCtrlQubits = op.getCtrlQubitOperands();
+        ValueRange parentOutNonCtrlQubits = parentOp.getNonCtrlQubitResults();
+        
+        bool foundNonCtrlMatch = false;
+        for (const auto &[idx, qubit] : llvm::enumerate(inNonCtrlQubits)) {
+            if (qubit == parentOutNonCtrlQubits[0]) {
+                foundNonCtrlMatch = true;
+                break;
+            }
+        }
 
+        bool foundCtrlMatch = false;
+        for (const auto &[idx, qubit] : llvm::enumerate(inCtrlQubits)) {
+            if (qubit == parentOutNonCtrlQubits[0]) {
+                foundCtrlMatch = true;
+                break;
+            }
+        }
+
+        // If neither the control nor non-control qubits match with parent's output,
+        // the pattern doesn't match
+        if (!foundNonCtrlMatch && !foundCtrlMatch) {
+            return failure();
+        }
+        
         dbgs() << "CNOT propagation pattern matched\n";
-        // Replace uses
+        return failure();
         std::vector<mlir::Value> originalQubits = parentOp.getQubitOperands();
 
         rewriter.replaceOp(op, originalQubits);
